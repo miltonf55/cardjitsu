e25dcf242d3404ea885f41dd1efe9a08a58d799d
Milton Ocampo <47097454+miltonf55@users.noreply.github.com>
2019-11-19 01:04:16 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+var app = express();
+const http = require("http").Server(app);
+const io = require("./libs/logic").listen(http);
+
+app.set("port", (process.env.PORT || 3001));
+app.use(express.static("public"));  
+
+app.get("/", function(req, res) {
+});
+
+app.get("*", function(req, res) {
+	res.status(404).send("Error 404 - No existe esa página my friend");
+});
+
+http.listen(app.get("port"), function() {
+	console.log("Servidor en el puerto", app.get("port"));
+});
